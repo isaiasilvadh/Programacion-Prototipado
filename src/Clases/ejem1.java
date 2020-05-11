15d18352bfe76d45cf5dae6465d8fc6e260e927b
isaiasilvadh <isaiasilva64@gmail.com>
2020-05-11 15:17:59 -0500

    ejemplo1

---
+++ src/Clases/ejem1.java
@@ -0,0 +1,17 @@
+package Clases;
+
+public class ejem1 {
+    public static void main(String[] args) {
+
+        int [] values = {};
+        for (int i=0; i<values.length; i++){
+            int index = -1;
+            for (int j=0; i<values.length; j++){
+                if (values[j] ==80){
+                    index =j;
+                }
+            }
+            break;
+        }
+    }
+}
