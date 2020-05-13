a6239be6124b46ce556f76efeb1f6d86d360a743
isaiasilvadh <isaiasilva64@gmail.com>
2020-05-12 22:25:59 -0500

    a

--- src/Clases/ejem1.java
+++ src/Clases/ejem1.java
@@ -1,17 +1,33 @@
 package Clases;
 
+import java.util.Scanner;
+
 public class ejem1 {
     public static void main(String[] args) {
-        int [] values = {};
-        for (int i = 0; i < 10000000; i++) {
-            int index = -1;
-            for (int j = 0; j < values.length; j++) {
-                if (values[j] == 80) {
-                    index = j;
-                    System.out.println("Elemto encontrado en la posicicion :" + index);
-                    break;
+
+            Scanner scan = new Scanner(System.in);
+            int values[], tamanio;
+            System.out.println("Tamaño del arreglo: ");
+            tamanio = scan.nextInt();
+            values = new int[tamanio];
+            for (int i = 0; i < values.length; i++) {
+                values[i] = (int) (Math.random() * 100);
+            }
+            for (int i = 0; i < 10000000; i++) {
+                int index = -1;
+                for (int j = 0; j < values.length; j++) {
+                    if (values[j] == 80) {
+                        index = j;
+                        System.out.println("Número encontrado -> en la posición" + index);
+                    }
+                    if (values[j] != 80) {
+                        System.out.println("No fue posible encontrar el número");
+
+                    }
                 }
             }
+
+
         }
     }
-}
+
