9f6a794387d6afc26dd17c9e464eb8374bfb452d
isaiasilvadh <isaiasilva64@gmail.com>
2020-05-12 20:32:39 -0500

    a

--- src/Clases/ejem1.java
+++ src/Clases/ejem1.java
@@ -2,16 +2,16 @@ package Clases;
 
 public class ejem1 {
     public static void main(String[] args) {
-
         int [] values = {};
-        for (int i=0; i<values.length; i++){
+        for (int i = 0; i < 10000000; i++) {
             int index = -1;
-            for (int j=0; i<values.length; j++){
-                if (values[j] ==80){
-                    index =j;
+            for (int j = 0; j < values.length; j++) {
+                if (values[j] == 80) {
+                    index = j;
+                    System.out.println("Elemto encontrado en la posicicion :" + index);
+                    break;
                 }
             }
-            break;
         }
     }
 }
